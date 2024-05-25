12f8b809d97e5b0a9ceff8f16bab673a493f52b0
aboufofana <aboufofana76@gmail.com>
2024-05-25 16:34:41 +0200

    Learn enums

---
+++ basic/enums.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type ServerState int
+
+const (
+	StateIdle = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "error",
+	StateRetrying:  "retrying",
+}
+
+func (state ServerState) String() string {
+	return stateName[state]
+}
+
+func main() {
+	ns := transition(StateIdle)
+	fmt.Println(ns)
+}
+
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknown state: %s", s))
+	}
+
+	return StateConnected
+}
