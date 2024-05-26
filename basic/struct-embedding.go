de53618b603797a8d13a37179e1b144cda52f50d
aboufofana <aboufofana76@gmail.com>
2024-05-26 17:04:02 +0200

    Learn embed struct and generic

---
+++ basic/struct-embedding.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type base struct {
+	num int
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base with num=%v", b.num)
+}
+
+type container struct {
+	base
+	str  string
+	test string
+}
+
+func main() {
+
+	co := container{
+		base: base{
+			num: 5,
+		},
+		str:  "some name",
+		test: "ahhh",
+	}
+
+	fmt.Printf("co={num: %v, str: %v}\n", co.num, co.str)
+
+	fmt.Println("also num:", co.base.num)
+
+	fmt.Println("describe:", co.describe())
+
+	type describer interface {
+		describe() string
+	}
+
+	var d describer = co
+	fmt.Println("describer:", d.describe())
+}
